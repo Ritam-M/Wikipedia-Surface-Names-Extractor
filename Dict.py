2e7cfed32987ad5f741453de3e3b06e27b1254cc
Ritam  Majumdar <hrickmajumdar@gmail.com>
2021-01-26 23:13:48 +0530

    Update Dict.py

--- Dict.py
+++ Dict.py
@@ -14,7 +14,9 @@ def gen_reader():
 sn_index = 0
 entity_index = 0
 SN_Dict = OrderedDict()
+SN_Freq = OrderedDict()
 Entity_Dict = OrderedDict()
+Entity_Freq = OrderedDict()
 
 # with open('Dict_en.json','r') as file:
 #    Dict = json.load(file)
@@ -37,19 +39,36 @@ for row in gen_reader():
     if Source not in Entity_Dict:
         entity_index += 1
         Entity_Dict[Source] = entity_index
+        Entity_Freq[Source] = 1
+    else:
+        Entity_Freq[Source] += 1
     
     if Dest not in Entity_Dict:
         entity_index += 1
         Entity_Dict[Dest] = entity_index
+        Entity_Freq[Dest] = 1
+    else:
+        Entity_Freq[Dest] += 1
     
     if surfaceName not in SN_Dict:
         sn_index+=1
         SN_Dict[surfaceName] = sn_index
-    
+        SN_Freq[surfaceName] = 1
+    else:
+        SN_Freq[surfaceName] += 1
+        
 with open('SN_Dict.json','w') as file:
     json.dump(SN_Dict, file)
     file.close()
 
+with open('SN_Freq.json','w') as file:
+    json.dump(SN_Freq, file)
+    file.close()
+
 with open('Entity_Dict.json','w') as file:
     json.dump(Entity_Dict, file)
+    file.close()
+
+with open('Entity_Freq.json','w') as file:
+    json.dump(Entity_Freq, file)
     file.close()
