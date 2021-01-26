0a358606922a7675f6673114555ebafc55472f06
Ritam  Majumdar <hrickmajumdar@gmail.com>
2021-01-26 22:11:10 +0530

    Update Dict.py
    SN, EN Dicts.

--- Dict.py
+++ Dict.py
@@ -2,49 +2,54 @@ import json
 import csv
 import sys
 import operator
+from collections import OrderedDict
 
-PATH_INPUT_SURFACE_NAMES = '/wikipedia/output2020/sv/surface_names.csv'
+PATH_INPUT_SURFACE_NAMES = '/wikipedia/output2020/en/fake/surface_names.csv'
 reader = csv.reader(open(PATH_INPUT_SURFACE_NAMES), delimiter=",")
 
 def gen_reader():
     for row in reader:
         yield row
 
-index = 0
-Dict = {}
+sn_index = 0
+entity_index = 0
+SN_Dict = OrderedDict()
+Entity_Dict = OrderedDict()
 
-with open('Dict_ru.json','r') as file:
-    Dict = json.load(file)
-    file.close()
+# with open('Dict_en.json','r') as file:
+#    Dict = json.load(file)
+#    file.close()
 
 for row in gen_reader():
-    index += 1
-    if index<=40000000:
+    try:
+        Source = row[1]
+    except IndexError:
         continue
-    else:
-        print(index/54237541)
-        try:
-            Name = row[2]
-        except IndexError:
-            continue
-        try:
-            surfaceName = row[3]
-        except IndexError:
-            continue
+    try:
+        Dest = row[2]
+    except IndexError:
+        continue
+    try:
+        surfaceName = row[3]
+    except IndexError:
+        continue
 
-        if Name not in Dict:
-            Dict[Name] = {}
-            Dict[Name][surfaceName]=1
-        else:
-            if surfaceName not in Dict[Name]:
-                Dict[Name][surfaceName]=1
-            else:
-                Dict[Name][surfaceName]+=1
+    if Source not in Entity_Dict:
+        entity_index += 1
+        Entity_Dict[Source] = entity_index
+    
+    if Dest not in Entity_Dict:
+        entity_index += 1
+        Entity_Dict[Dest] = entity_index
+    
+    if surfaceName not in SN_Dict:
+        sn_index+=1
+        SN_Dict[surfaceName] = sn_index
+    
+with open('SN_Dict.json','w') as file:
+    json.dump(SN_Dict, file)
+    file.close()
 
-        if(index%10000000==0):
-            with open('Dict_sv.json','w') as file:
-                json.dump(Dict, file)
-                file.close()
-with open('Dict_sv.json','w') as file:
-    json.dump(Dict, file)
+with open('Entity_Dict.json','w') as file:
+    json.dump(Entity_Dict, file)
     file.close()
